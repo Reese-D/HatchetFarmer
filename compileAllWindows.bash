56fe48f81997b816d9ef542cb00dfd20a36f5ff5
Miles <dewinmil@mail.gvsu.edu>
2015-11-19 16:38:59 -0500

    got rid of old lib2

--- compileAllWindows.bash
+++ compileAllWindows.bash
@@ -1,4 +1,4 @@
 g++ -c Square.cpp -Wall
 g++ Grid.cpp -c -Wall
 g++ GridTest.cpp -o gridTest.out -Wall Grid.o Square.o
-g++ -L./lib2 -I./include -lsfml-window -lsfml-graphics -lsfml-system Test.cpp -o Test.out -Wall
+g++ -L./lib2 -I./include -llibsfml-window -llibsfml-graphics -llibsfml-system Test.cpp -o Test.out -Wall
