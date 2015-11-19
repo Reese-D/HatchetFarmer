e3597e492fb4ec26437003540dbc8f5d6200d215
Reese-D <reese.dewind@gmail.com>
2015-11-19 16:56:23 -0500

    small change to compileWindows

--- compileAllWindows.bash
+++ compileAllWindows.bash
@@ -1,5 +1,5 @@
 g++ -c boardObject.cpp -Wall
 g++ -I./include -c Square.cpp -Wall
 g++ -I./include Grid.cpp -c -Wall
-g++ -I./include -L./lib -lsfml-window -lsfml-graphics -lsfml-system GridTest.cpp -o gridTest.out -Wall Grid.o Square.o
+g++ -I./include -L./lib ./lib2/libsfml-window.a ./lib2/libsfml-graphics.a ./lib2/libsfml-system.a GridTest.cpp -o gridTest.out -Wall Grid.o Square.o
 g++ -L./lib2 -I./include ./lib2/libsfml-window.a ./lib2/libsfml-graphics.a ./lib2/libsfml-system.a Test.cpp -o Test.out -Wall Grid.o Square.o
