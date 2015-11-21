39d70e2261621ef2e7cf2ef3c1eba98644266747
Miles <dewinmil@mail.gvsu.edu>
2015-11-20 19:15:47 -0500

    Fixed for windowsgit statusgit status!

--- compileAllWindows.bash
+++ compileAllWindows.bash
@@ -1,5 +1,5 @@
 g++ -c boardObject.cpp -Wall
 g++ -I./include -c Square.cpp -Wall
 g++ -I./include Grid.cpp -c -Wall
-g++ -I./include -L./lib ./lib2/libsfml-window.a ./lib2/libsfml-graphics.a ./lib2/libsfml-system.a GridTest.cpp -o gridTest.out -Wall Grid.o Square.o
-g++ -L./lib2 -I./include ./lib2/libsfml-window.a ./lib2/libsfml-graphics.a ./lib2/libsfml-system.a Test.cpp -o Test.out -Wall Grid.o Square.o
+g++ -I./include -L./lib2 -lsfml-window-2 -lsfml-graphics-2 -lsfml-system-2 GridTest.cpp -o gridTest.out -Wall Grid.o Square.o
+g++ -L./lib2 -I./include -lsfml-window-2 -lsfml-graphics-2 -lsfml-system-2 Test.cpp -o Test.out -Wall Grid.o Square.o
